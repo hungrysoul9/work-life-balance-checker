4d4bf33e14ecda5087511ab579bc9478fa618bc7
hd-mac <>
2018-07-22 14:46:35 +0900

    [ios] 필요없는 주석 제거

--- ios/work-life-balance-checker/work-life-balance-checker/TimerViewController.swift
+++ ios/work-life-balance-checker/work-life-balance-checker/TimerViewController.swift
@@ -15,13 +15,10 @@ class TimerViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     @objc func onTimerCallback() {
